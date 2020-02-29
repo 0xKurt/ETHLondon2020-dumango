a291a687d11b881b389034050378db408a460956
Kurt Merbeth <Kurt.Me@mail.ru>
2020-02-29 17:13:57 +0100

    final sol v 0.1 :D

--- pigleth/contract/Pigleth.sol
+++ pigleth/contract/Pigleth.sol
@@ -19,6 +19,7 @@ contract Pigleth {
         fu.setMonthlyAmount(_monthlyAmount);
         fu.setPiglethAddress(address(this));
         fu.setDate(d.day, d.month, d.year);
+        fu.setPensionYear(_pensionAge-_currentAge);
         userFundMapping[msg.sender] = address(fu);
         return address(fu);
     }
@@ -32,8 +33,19 @@ contract Pigleth {
     function fund() public payable {
         address fuAddress = userFundMapping[msg.sender];
         Fund fu = Fund(fuAddress);
-        if(msg.value == fu.getMonthlyAmount()) {
+        if(msg.value == fu.getMonthlyAmount() && d.month > fu.getLastPaymentMonth()) {
             fu.setPaidAmount(fu.getPaidAmount()+msg.value);
+            fu.setLastPayment(d.day, d.month, d.year);
+        }
+    }
+
+    function payPension() public isPension() payable {
+        address fuAddress = userFundMapping[msg.sender];
+        Fund fu = Fund(fuAddress);
+        if(d.month > fu.getLastPensionMonth()) {
+            uint amount = fu.getPaidAmount()/fu.getPensionYear()/12;
+            msg.sender.transfer(amount);
+            fu.setLastPension(d.day, d.month, d.year);
         }
     }
 
@@ -41,6 +53,12 @@ contract Pigleth {
         Fund fu = Fund(userFundMapping[msg.sender]);
         msg.sender.transfer(fu.getPaidAmount());
         fu.setPaidAmount(0);
+    }
+
+    modifier isPension() {
+        Fund fu = Fund(userFundMapping[msg.sender]);
+        require(fu.getPensionYear()+fu.getYear() == d.year);
+        _;
     }
 
     modifier isOwner() {
@@ -68,14 +86,29 @@ contract Fund {
     uint public monthlyAmount;
     uint public paidAmount;
     date public d;
+    date public lastPayment;
+    date public lastPension;
+    uint public pensionYear;
 
     constructor() public {
         owner = msg.sender;
-        pig = Pigleth(msg.sender); // what happend here?
+        pig = Pigleth(msg.sender); // what happen here?
         paidAmount = 0;
         monthlyAmount = 0;
         pensionAge = 0;
         currentAge = 0;
+    }
+
+    function setLastPayment(uint _day, uint _month, uint _year) public isOwner() {
+        lastPayment.day = _day;
+        lastPayment.month = _month;
+        lastPayment.year = _year;
+    }
+
+    function setLastPension(uint _day, uint _month, uint _year) public isOwner() {
+        lastPension.day = _day;
+        lastPension.month = _month;
+        lastPension.year = _year;
     }
 
     function setDate(uint _day, uint _month, uint _year) public isOwner() {
@@ -101,11 +134,52 @@ contract Fund {
     function setPiglethAddress(address _piggy) public {
         piglethAddress = _piggy;
     }
+    function setPensionYear(uint _pensionYear) public isOwner() {
+        pensionYear = _pensionYear;
+    }
+    function getPensionYear() public returns(uint) {
+        return pensionYear;
+    }
     function getPaidAmount() public isOwner() returns (uint) {
         return paidAmount;
     }
     function getMonthlyAmount() public returns (uint) {
         return monthlyAmount;
+    }
+
+    function getPensionAge() public returns (uint) {
+        return pensionAge;
+    }
+    function getCurrentAge() public returns (uint) {
+        return currentAge;
+    }
+
+    function getDay() public returns (uint) {
+        return d.day;
+    }
+    function getMonth() public returns (uint) {
+        return d.month;
+    }
+    function getYear() public returns (uint) {
+        return d.year;
+    }
+    function getLastPaymentDay() public returns (uint) {
+        return lastPayment.day;
+    }
+    function getLastPaymentMonth() public returns (uint) {
+        return lastPayment.month;
+    }
+    function getLastPaymentYear() public returns (uint) {
+        return lastPayment.year;
+    }
+        function getLastPensionDay() public returns (uint) {
+        return lastPayment.day;
+    }
+    function getLastPensionMonth() public returns (uint) {
+        return lastPayment.month;
+    }
+    function getLastPensionYear() public returns (uint) {
+        return lastPayment.year;
     }
 
     modifier isOwner() {
