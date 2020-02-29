6f03e9d9b1549b0343630ba30c89cdc2ca6e55ef
Valerie <valerie.vaske@gmail.com>
2020-02-29 19:01:13 +0100

    Added Footer
    need help with footer styling

---
+++ pigleth/src/components/Footer.js
@@ -0,0 +1,15 @@
+// dumango
+import React, { Component } from 'react';
+import '../App.css';
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <div class="footer">
+          Trust in piggy!
+      </div>
+    );
+  }
+}
+export default Footer;
