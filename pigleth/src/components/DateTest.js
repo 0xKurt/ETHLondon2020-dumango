666ad96f912e4f254f4df94ed8cd4d4a279df28a
Valerie <valerie.vaske@gmail.com>
2020-02-29 19:01:32 +0100

    Rumspielen mit DateTest
    for pitch

---
+++ pigleth/src/components/DateTest.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class DateTest extends Component {
+
+  render() {
+    return (
+      <div class="date-test">
+
+      </div>
+    );
+  }
+
+}
+
+export default DateTest;
